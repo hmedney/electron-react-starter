acb835b15168f4243975d19da3a2398625e684fc
Hunter Medney <hunter_medney@us.ibm.com>
2020-11-24 02:24:00 -0500

    explicitly set contextIsolation flag see https://github.com/electron/electron/issues/23506

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ function createWindow() {
     width: 1024,
     height: 768,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false
     }
   });
 
